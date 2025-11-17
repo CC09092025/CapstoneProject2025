60ba4fed39383053a38a3ac13be444b55000adfd
tou - ThatOneUnoriginal <70731807+ThatOneUnoriginal@users.noreply.github.com>
2025-11-17 15:34:42 -0500

    Added PIN functionality

--- CapstoneProject2025/App.xaml.cs
+++ CapstoneProject2025/App.xaml.cs
@@ -1,11 +1,14 @@
-﻿namespace CapstoneProject2025
+﻿using CapstoneProject2025.Services;
+
+namespace CapstoneProject2025
 {
     public partial class App : Application
     {
+        public static bool IsAuthenticated { get; set; } = false;
+
         public App()
         {
             InitializeComponent();
-
             MainPage = new MainPage();
         }
     }
